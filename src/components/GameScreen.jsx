ecbd0623a39ea393b9f15645e428efe0671e8b35
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2024-03-11 12:54:47 -0300

    jsx game

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -2,9 +2,27 @@ import './StartScreen.css';
 
 const GameScreen = ({ verify }) => {
 	return (
-		<div>
-			<h1>Game Screen</h1>
-			<button onClick={verify}>End Game</button>
+		<div className='game_container'>
+			<p className='points'>
+				<span>Points: 000</span>
+			</p>
+			<h1>Guess the word</h1>
+			<h3 className='tip'>Tip: DICA</h3>
+			<div className='word_container'>
+				<span className='letter'>A</span>
+				<span className='blank'></span>
+			</div>
+			<div className='letter-container'>
+				<form>
+					<input type='text' name='letter' maxLength='1' required />
+					<button>Ok</button>
+				</form>
+			</div>
+			<div className='wrong_letter'>
+				<p>Wrong Letter: </p>
+				<span>a, </span>
+				<span>b, </span>
+			</div>
 		</div>
 	);
 };
