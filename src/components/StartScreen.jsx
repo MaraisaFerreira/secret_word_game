6c5e975a87c327e9da1023a4c5a7cc8e6af1167a
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2024-03-11 11:05:41 -0300

    start screen

---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,12 @@
+import './StartScreen.css';
+
+const StartScreen = () => {
+	return (
+		<div className='start_screen'>
+			<h1>Secret Word</h1>
+			<button>Start Game</button>
+		</div>
+	);
+};
+
+export default StartScreen;
