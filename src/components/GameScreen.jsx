ddc5303834959b01ea695cf2859bc1472ba28e45
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2024-03-11 13:12:01 -0300

    game screen css

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -1,4 +1,4 @@
-import './StartScreen.css';
+import './GameScreen.css';
 
 const GameScreen = ({ verify }) => {
 	return (
@@ -7,12 +7,15 @@ const GameScreen = ({ verify }) => {
 				<span>Points: 000</span>
 			</p>
 			<h1>Guess the word</h1>
-			<h3 className='tip'>Tip: DICA</h3>
+			<h3 className='tip'>
+				Tip: <span>DICA</span>
+			</h3>
 			<div className='word_container'>
 				<span className='letter'>A</span>
 				<span className='blank'></span>
 			</div>
-			<div className='letter-container'>
+			<div className='letter_container'>
+				<p>Pick a letter</p>
 				<form>
 					<input type='text' name='letter' maxLength='1' required />
 					<button>Ok</button>
